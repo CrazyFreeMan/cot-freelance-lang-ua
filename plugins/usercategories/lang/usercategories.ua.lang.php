fe6ef365428480ee598ed9fd8bde5b2986a9c686
CrazyFreeMan <yaroslav.romanenko@gmail.com>
2015-06-04 13:09:00 +0300

    fixed #5

--- plugins/usercategories/lang/usercategories.ua.lang.php
+++ plugins/usercategories/lang/usercategories.ua.lang.php
@@ -7,7 +7,6 @@
  */
 defined('COT_CODE') or die('Wrong URL.');
 
-
-$L['usercategories_cat_editor'] = 'Редактор категорій фрілансерів';
-$L['usercategories_in_cat'] = 'Категорії';
-
+$L['usercategories_admin_config_groupid'] = 'Група';
+$L['usercategories_admin_config_limit'] = 'Ліміт';
+$L['usercategories_error_catslimit'] = 'Ви можете вибрати не більше {$limit} спеціалізацій';
