24ff3b82b3c92d1ecc5366cf616c8fe7509e4a5b
MyBe <ostap4ukigor@i.ua>
2015-12-13 13:36:08 +0200

    Українська локалізація main
    Українська локалізація

---
+++ lang/ua/main.ua.lang.php
@@ -0,0 +1,516 @@
+<?php
+/**
+ * Ukrainian Language File (main.ua.lang.php)
+ *
+ * @package Cotonti
+ * @copyright (c) Cotonti Team
+ * @license https://github.com/Cotonti/Cotonti/blob/master/License.txt
+ * @Ukrainian translate by Igor Ostapchuk
+ */
+
+defined('COT_CODE') or die('Wrong URL.');
+
+/**
+ * Main strings
+ */
+
+$L['Access'] = 'Доступ';
+$L['Action'] = 'Дія';
+$L['Active'] = 'Активно';
+$L['Activity'] = 'Активність';
+$L['Add'] = 'Добавити';
+$L['Added'] = 'Добавлено';
+$L['Admin'] = 'Адміністратор';
+$L['Administration'] = 'Адміністрування';
+$L['Adminpanel'] = 'Керування сайтом';
+$L['Age'] = 'Вік';
+$L['Ago'] = 'назад';
+$L['AIM'] = 'Адреса AIM';
+$L['Alias'] = 'Аліас';
+$L['All'] = 'Всі';
+$L['Allcategories'] = 'Всі категорії';
+$L['Allpages'] = 'Всі сторінки';
+$L['Alltimesare'] = 'Дата вказана в';
+$L['Anonymous'] = 'Анонімний';
+$L['Archives'] = 'Архів';
+$L['Ascending'] = 'По віку';
+$L['Author'] = 'Автор';
+$L['Available'] = 'Доступно';
+$L['Avatar'] = 'Аватар';
+$L['Avatars'] = 'Аватари';
+$L['Banned'] = 'Заблоковані';
+$L['BBcode'] = 'BBCode';
+$L['BBcodes'] = 'BBCodes';
+$L['Begin'] = 'Опубліковано з';
+$L['Birthdate'] = 'Дата народження';
+$L['Birthday'] = 'День народження';
+$L['Blog'] = 'Блог';
+$L['Blogs'] = 'Блоги';
+$L['Body'] = 'Текст';
+$L['Byfirstletter'] = 'По першому символу';
+$L['bytes'] = 'байт';
+$L['Cancel'] = 'Відміна';
+$L['Categories'] = 'Категорії';
+$L['Category'] = 'Категорія';
+$L['Clearstatus'] = 'Очистити статус';
+$L['Clickhere'] = 'Продовжити...';
+$L['Close'] = 'Закрити';
+$L['Code'] = 'Код';
+$L['Configuration'] = 'Конфігурація';
+$L['Confirm'] = 'Підтвердити';
+$L['Copyright'] = 'Копірайт';
+$L['Core'] = 'Ядро';
+$L['Count'] = 'Кількість';
+$L['Country'] = 'Країна';
+$L['Create'] = 'Створити';
+$L['Crumbs'] = 'Навігація';
+$L['Custom'] = 'Користувацький';
+$L['Database'] = 'База даних';
+$L['Date'] = 'Дата';
+$L['Days'] = 'Дні';
+$L['Default'] = 'По замовчуванню';
+$L['Delete'] = 'Видалити';
+$L['Deleted'] = 'Видалено';
+$L['Descending'] = 'По спаданню';
+$L['Description'] = 'Опис';
+$L['Disabled'] = 'Відключено';
+$L['Done'] = 'Виконано';
+$L['Download'] = 'Завантажити';
+$L['Downloaded'] = 'Завантажено';
+$L['Duplicate'] = 'Дублювати';
+$L['Duration'] = 'Протяжність';
+$L['Edit'] = 'Правка';
+$L['Email'] = 'E-mail';
+$L['Enabled'] = 'Включено';
+$L['Entry'] = 'Запис';
+$L['Entries'] = 'Записи';
+$L['Error'] = 'Помилка';
+$L['Event'] = 'Подія';
+$L['Events'] = 'Події';
+$L['Expand'] = 'Розгорнути';
+$L['Expire'] = 'Опубліковано по';
+$L['Extrafield'] = 'Додаткове поле';
+$L['Filedunder'] = 'Опубліковано в';
+$L['File'] = 'Файл';
+$L['Files'] = 'Файли';
+$L['Filesize'] = 'Розмір файла';
+$L['Filter'] = 'Фільтр';
+$L['Filters'] = 'Фільтри';
+$L['Fold'] = 'Згорнути';
+$L['Folder'] = 'Каталог';
+$L['Folders'] = 'Каталоги';
+$L['Forums'] = 'Форуми';
+$L['Found'] = 'Знайдено';
+$L['Galleries'] = 'Галереї';
+$L['Gallery'] = 'Галерея';
+$L['Gender'] = 'Стать';
+$L['Ghost'] = 'Режим Неведимки';
+$L['Group'] = 'Група';
+$L['Group0'] = 'Прихована група';
+$L['Group1'] = 'Відкрита група';
+$L['Groups'] = 'Групи';
+$L['Groupsmembership'] = 'Відноситься до груп';
+$L['Guest'] = 'Гість';
+$L['Guests'] = 'Гості';
+$L['Headlines'] = 'Тільки заголовки';
+$L['Help'] = 'Допомога';
+$L['Here'] = 'Тут';
+$L['Hi'] = 'Здоровенькі були!';
+$L['Hidden'] = 'Приховано';
+$L['Hide'] = 'Приховати';
+$L['Hits'] = 'Перегляди';
+$L['Home'] = 'Головна';
+$L['Hook'] = 'Хук';
+$L['Hooks'] = 'Хуки';
+$L['Hours'] = 'Години';
+$L['Icon'] = 'Піктограма (URL)';
+$L['ICQ'] = 'ICQ';
+$L['Id'] = 'ID';
+$L['Image'] = 'Зображення';
+$L['ImageURL'] = 'URL зображення';
+$L['Inactive'] = 'Неактивовані';
+$L['Ip'] = 'IP-адреса';
+$L['Ipsearch'] = 'Пошук по IP';
+$L['IRC'] = 'IRC';
+$L['Item'] = 'Елемент';
+$L['Items'] = 'Елементи';
+$L['JustNow'] = 'Тольки що';
+$L['kb'] = 'Кб';
+$L['Key'] = 'Код';
+$L['Keywords'] = 'Ключові слова';
+$L['Language'] = 'Мова';
+$L['Languages'] = 'Мови';
+$L['Lastlogged'] = 'Остання авторизація';
+$L['Lastpost'] = 'Останнє повідомлення сообщение';
+$L['Left'] = 'По лівому краю';
+$L['LessThan'] = 'Меньше чим';
+$L['Level'] = 'Рівень';
+$L['Levels'] = 'Рівні';
+$L['Link'] = 'Лінк';
+$L['Links'] = 'Лінки';
+$L['Locale'] = 'Локалізація';
+$L['Location'] = 'Місто';
+$L['Lock'] = 'Заблокуват';
+$L['Locked'] = 'Заблоковано';
+$L['Log'] = 'Лог';
+$L['Login'] = 'Вхід';
+$L['Logout'] = 'Вихід';
+$L['Main'] = 'Головна';
+$L['Maingroup'] = 'Основна група';
+$L['Member'] = 'Учасник';
+$L['Members'] = 'Учасники';
+$L['Members_download'] = 'Цей файл доступний тільки для зареєстрованих користувачів';
+$L['Members_only'] = 'Тільки для зареєстрованих користувачів';
+$L['Menu'] = 'Меню';
+$L['Menus'] = 'Слоти для меню';
+$L['Message'] = 'Повідомлення';
+$L['Messages'] = 'Повідомлення';
+$L['Minlevel'] = 'Мін. рівень';
+$L['Minutes'] = 'Хвилини';
+$L['Moderators'] = 'Модератори';//тут пауза в 23:50
+$L['Module'] = 'Модуль';
+$L['Modules'] = 'Модулі';
+$L['Month'] = 'місяць';
+$L['Months'] = 'місяці';
+$L['More'] = 'Детальніше...';
+$L['Move'] = 'Перемістити';
+$L['Moved'] = 'Було переміщено';
+$L['MSN'] = 'MSN Messenger';
+$L['Myjournal'] = 'Мої щоденники';
+$L['Mypfs'] = 'Мої файли';
+$L['na'] = 'Недоступно';
+$L['nf'] = 'Не знайдено';
+$L['Name'] = 'Ім"я';
+$L['Names'] = 'Імена';
+$L['Navigation'] = 'Навігація';
+$L['New'] = 'Новий';
+$L['News'] = 'Новини';
+$L['Newspost'] = 'Повідомлення новини';
+$L['Newsposts'] = 'Повідемлення новин';
+$L['Next'] = 'Наступна сторінка';
+$L['No'] = 'Ні';
+$L['Noitemsfound'] = 'За вашим запитом нічого не знайдено';
+$L['None'] = 'Немає записів';
+$L['Normal'] = 'Норм.';
+$L['Notes'] = 'Примітки';
+$L['No_items'] = 'Немає елементів';
+$L['Occupation'] = 'Професія';
+$L['Of'] = 'з'; // New in N-x.x.x
+$L['Offline'] = 'Немає в мережі';
+$L['Online'] = 'В мережі';
+$L['Onpage'] = 'На сторінці';
+$L['Ontop'] = 'Вгору';
+$L['Open'] = 'Відкрити';
+$L['Option'] = 'Опція';
+$L['Options'] = 'Опції';
+$L['Order'] = 'Порядок';
+$L['Ordered'] = 'впорядковано';
+$L['Originalmessage'] = 'Вихідне повідомлення';
+$L['Other'] = 'Інше';
+$L['Overview'] = 'Карта сайту';
+$L['Owner'] = 'Власник';
+$L['Page'] = 'Сторінка';
+$L['Pages'] = 'Сторінки';
+$L['Parser'] = 'Парсер';
+$L['Part'] = 'Частина';
+$L['Parts'] = 'Частини';
+$L['Password'] = 'Пароль';
+$L['Path'] = 'Шлях';
+$L['PFS'] = 'Мої файли';
+$L['Photo'] = 'Фото';
+$L['Photos'] = 'Фото';
+$L['Picture'] = 'Зображення';
+$L['Pictures'] = 'Зображення';
+$L['Plugin'] = 'Плагін';
+$L['Plugins'] = 'Плагіни';
+$L['Poll'] = 'Опитування';
+$L['Polls'] = 'Опитування';
+$L['Poster'] = 'Автор';
+$L['Preview'] = 'Попередній перегляд';
+$L['Previous'] = 'Поперелня сторінка';
+$L['Private_Messages'] = 'Особисті повідомлення';
+$L['Profile'] = 'Мої налаштування';
+$L['Prune'] = 'Очистити';
+$L['Public'] = 'Для всіх';
+$L['Publish'] = 'Опубліковати';
+$L['Putinvalidationqueue'] = 'В чергу на підтвердження';
+$L['Quote'] = 'Цитата';
+$L['Rank'] = 'Рейтинг';
+$L['Rated'] = 'Рейтинг';
+$L['Rating'] = 'Рейтинг';
+$L['Ratings'] = 'Рейтинги';
+$L['Read'] = 'Зчитування';
+$L['ReadMore'] = 'Детально';
+$L['Reason'] = 'Причина';
+$L['Recipient'] = 'Отримувач';
+$L['Recipients'] = 'Отримувачі';
+$L['Refresh'] = 'Обновити';
+$L['Register'] = 'Зареєструватися';
+$L['Registered'] = 'Дата реєстрації';
+$L['Registration'] = 'Реєстрація';
+$L['Remove'] = 'Видалити';
+$L['Replies'] = 'Відповіді';
+$L['Reply'] = 'Відповідь';
+$L['Reset'] = 'Скинути';
+$L['Restore'] = 'Відновити';
+$L['Resync'] = 'Синхронізувати';
+$L['Resynced'] = 'Синхронізовано';
+$L['Right'] = 'По правому краю';
+$L['Rights'] = 'Права';
+$L['RSS_Feeds'] = 'RSS канали';
+$L['Save'] = 'Зберегти';
+$L['Saveasdraft'] = 'Зберегти чорновик';
+$L['Saved'] = 'Збережено';
+$L['Scheme'] = 'Кольорова схема';
+$L['Schemes'] = 'Кольорові схеми';
+$L['Search'] = 'Пошук';
+$L['Seconds'] = 'Секунди';
+$L['Section'] = 'Разділ';
+$L['Sections'] = 'Разділи';
+$L['Security'] = 'Безпека';
+$L['Sender'] = 'Відправник';
+$L['Senders'] = 'Відправники';
+$L['Session_expired'] = 'Час вашої сесії вийшов. Спробуйте ще раз.';
+$L['Set'] = 'Встановити';
+$L['Settings'] = 'Налаштування';
+$L['Setup'] = 'Налаштувати';
+$L['SFS'] = 'Файловий архів сайту';
+$L['Show'] = 'Показати';
+$L['Signature'] = 'Підпис';
+$L['Signatures'] = 'Підписи';
+$L['Size'] = 'Розмір';
+$L['Smilies'] = 'Смайлики';
+$L['Spoiler'] = 'Спойлер';
+$L['Standalone'] = 'Standalone';
+$L['Started'] = 'Створена';
+$L['Statistics'] = 'Статистика';
+$L['StatsLogs'] = 'Статистика та логи';
+$L['Status'] = 'Статус';
+$L['Structure'] = 'Структура';
+$L['Subforum'] = 'Підфорум';
+$L['Subforums'] = 'Підфоруми';
+$L['Subject'] = 'Тема';
+$L['Submit'] = 'Відправити';
+$L['Submitforapproval'] = 'Відправити на підтвердження';
+$L['Submitnew'] = 'Створити нову сторінку';
+$L['Submittedby'] = 'Опубліковано';
+$L['Summary'] = 'Зміст';
+$L['System'] = 'Система';
+$L['Tag'] = 'Тег';
+$L['Tags'] = 'Теги';
+$L['Text'] = 'Текст';
+$L['Theme'] = 'Тема';
+$L['Themes'] = 'Теми';
+$L['Thumbnail'] = 'Мініатюра';
+$L['Thumbnails'] = 'Мініатюри';
+$L['Timezone'] = 'Часовий пояс';
+$L['Title'] = 'Заголовок';
+$L['Today'] = 'Сьогодні';
+$L['Tool'] = 'Утилита';
+$L['Tools'] = 'Утилити';
+$L['Total'] = 'Всього';
+$L['TPL'] = 'TPL';
+$L['Topic'] = 'Тема';
+$L['Type'] = 'Тип';
+$L['Unfold'] = 'Разгорнути';
+$L['Unknown'] = '?[Unknown]';
+$L['Unlock'] = 'Разблокувати';
+$L['Unlocked'] = 'Разблоковано';
+$L['Until'] = 'До';
+$L['Update'] = 'Обновити';
+$L['Updated'] = 'Обновлено';
+$L['Upload'] = 'Завантажити';
+$L['Uploaded'] = 'Завантажено';
+$L['URL'] = 'URL';
+$L['User'] = 'Користувач';
+$L['Userid'] = 'ID користувача';
+$L['Username'] = 'Імя';
+$L['Users'] = 'користувачі';
+$L['Validate'] = 'Підтвердити';
+$L['Value'] = 'Значення';
+$L['Values'] = 'Значення';
+$L['Version'] = 'Версія';
+$L['View'] = 'Перегляд';
+$L['Views'] = 'Переглляди';
+$L['Vote'] = 'Голос';
+$L['Votes'] = 'Голоси';
+$L['Warning'] = 'Увага';
+$L['Website'] = 'Веб-сайт';
+$L['Week'] = 'неділя';
+$L['Weeks'] = 'неділі';
+$L['Wipe'] = 'Видалити остаточно';
+$L['Wipeall'] = 'Видалити остаточно ВСЕ';
+$L['Write'] = 'Запис';
+$L['Yes'] = 'Так';
+$L['YIM'] = 'Yahoo Messenger';
+
+/**
+ * cot_declension arrays
+ */
+$Ls['Folders'] = "каталог,каталогу,каталогів";
+$Ls['Files'] = "файл,файлу,файлів";
+
+$Ls['Guests'] = "гість,гістя,гостей";
+$Ls['Members'] = "користувач,користувача,користувачів";
+$Ls['Pages'] = "сторінка,сторінки,сторінок";
+$Ls['Times'] = "раз,разу,разів";
+
+$Ls['Milliseconds'] = "мс,мс,мс";
+$Ls['Seconds'] = "секунда,секунди,секунд";
+$Ls['Minutes'] = "хвилина,хвилини,хвилин";
+$Ls['Hours'] = "година,годину,годин";
+$Ls['Days'] = "день,дня,днів";
+$Ls['Weeks'] = "неділя,неділі,неділь";
+$Ls['Months'] = "місяць,місяця,місяців";
+$Ls['Years'] = "Рік,року,років";
+
+$Ls['Bytes'] = "B,B";
+$Ls['Kilobytes'] = "kB,kB";
+$Ls['Megabytes'] = "MB,MB";
+$Ls['Gigabytes'] = "GB,GB";
+$Ls['Terabytes'] = "TB,TB";
+$Ls['Kibibytes'] = "KiB,KiB";
+$Ls['Mebibytes'] = "MiB,MiB";
+$Ls['Gibibytes'] = "GiB,GiB";
+$Ls['Tebibytes'] = "TiB,TiB";
+
+/**
+ * Date / Time formats
+ */
+
+$Ldt['date_full'] = 'd.m.Y';
+$Ldt['date_medium'] = 'm.Y';
+$Ldt['date_short'] = 'd.m';
+$Ldt['date_text'] = 'd F Y';
+$Ldt['date_fulltext'] = 'l, d F Y';
+$Ldt['time_full'] = 'H:i:s';
+$Ldt['time_medium'] = 'G:i';
+$Ldt['time_short'] = 'i:s';
+$Ldt['time_text'] = 'g:i A';
+$Ldt['time_fulltext'] = 'g:i:s A';
+$Ldt['datetime_full'] = 'd.m.Y H:i:s';
+$Ldt['datetime_medium'] = 'd.m.Y H:i';
+$Ldt['datetime_short'] = 'd.m H:i';
+$Ldt['datetime_text'] = 'd F Y H:i';
+$Ldt['datetime_fulltext'] = 'l, d F Y H:i';
+$Ldt['week_full'] = 'o-\WW';
+$Ldt['week_medium'] = '\WW';
+$Ldt['week_short'] = '\WW-N';
+$Ldt['week_text'] = '\WW, l';
+$Ldt['week_fulltext'] = 'o-\WW, l';
+
+/**
+ * Days of the week
+ */
+
+$L['Monday'] = 'Понеділок';
+$L['Tuesday'] = 'Вівторок';
+$L['Wednesday'] = 'Середа';
+$L['Thursday'] = 'Четвер';
+$L['Friday'] = 'Пятниця';
+$L['Saturday'] = 'Субота';
+$L['Sunday'] = 'Неділя';
+$L['Monday_s'] = 'пн';
+$L['Tuesday_s'] = 'вт';
+$L['Wednesday_s'] = 'ср';
+$L['Thursday_s'] = 'чт';
+$L['Friday_s'] = 'пт';
+$L['Saturday_s'] = 'сб';
+$L['Sunday_s'] = 'нд';
+
+/**
+ * Months
+ */
+
+$L['January'] = 'січень';
+$L['February'] = 'лютий';
+$L['March'] = 'березень';
+$L['April'] = 'квітень';
+$L['May'] = 'травень';
+$L['June'] = 'червень';
+$L['July'] = 'липень';
+$L['August'] = 'серпень';
+$L['September'] = 'вересень';
+$L['October'] = 'жовтень';
+$L['November'] = 'листопад';
+$L['December'] = 'грудень';
+$L['January_s'] = 'січ';
+$L['February_s'] = 'лют';
+$L['March_s'] = 'бер';
+$L['April_s'] = 'кві';
+$L['May_s'] = 'тра';
+$L['June_s'] = 'чер';
+$L['July_s'] = 'лип';
+$L['August_s'] = 'сер';
+$L['September_s'] = 'вер';
+$L['October_s'] = 'жов';
+$L['November_s'] = 'лис';
+$L['December_s'] = 'гру';
+
+/**
+ * Number formatting
+ */
+
+$Ln['decimal_point'] = ',';
+$Ln['thousands_separator'] = ' ';
+
+/**
+ * Gender
+ */
+
+$L['Gender_F'] = 'Жіночий';
+$L['Gender_M'] = 'Чоловічий';
+$L['Gender_U'] = 'Не вказаний';
+
+/**
+ * Common Entries
+ */
+
+$L['com_themefail'] = 'Помилка завантаження вибраної теми: обновіть або змініть особисті налаштування';
+$L['com_schemefail'] = 'Помилка завантаження кольорової схеми вибраної теми: обновіть або змініть особисті налаштування';
+$L['com_defthemefail'] = 'Помилка, не знайдено стандартну тему оформлення, перевірте файли в каталозі з темами';
+
+/**
+ * Header
+ */
+
+$L['hea_noprivatemessages'] = 'Особистих повідомлень немає';
+$L['hea_viewsite'] = 'Перегляд сайту';
+$L['hea_valqueues'] = 'В черзі на підтвердження: '; // Discuss
+$L['hea_welcome'] = 'Ласкаво просимо';
+$L['hea_youareloggedas'] = 'Користувач';
+$L['hea_youarenotlogged'] = 'Ви не авторизовані';
+
+/**
+ * Footer
+ */
+
+$L['foo_poweredby'] = 'Працюємо на';
+$L['foo_created'] = 'Сторінка створена за';
+$L['foo_sqlqueries'] = 'SQL запросів';
+$L['foo_sqltotal'] = 'Загальний час SQL';
+$L['foo_sqlaverage'] = 'середній час SQL';
+
+/**
+ * Extra fields
+ */
+
+$L['field_pregmatch'] = 'Значення поля не вірне';
+$L['field_notinarray'] = 'Значення поля не вірне';
+$L['field_required'] = 'Поле повинне бути заповнено';
+
+/**
+ * Miscellaneous
+ */
+
+$L['pagenav_first'] = '&lt;&lt;';
+$L['pagenav_prev'] = '&lt;';
+$L['pagenav_next'] = '&gt;';
+$L['pagenav_last'] = '&gt;&gt;';
+
+$L['ajaxSenderror'] = 'На сервері трапилася внутрішня помилка або помилка конфігурації, в результатя чого ваш запит не був виконаний. Звяжіться з адміністратором сайту і проінформуйте його в тому, коли виникла помилка і які з ваших дій могли її викликати.';
+
+$L['captcha_verification_failed'] = 'Невірний код захисту від роботів';
+
+$L['shield_title'] = 'Захист від hammering-атак';
+$L['shield_protect'] = 'Анти-хаммеринг активований, спопробуйте знову через {$sec} секунд...<br />Післе цього проміжку часу ви зможете оновити дану сторінку і продовжити.<br />Остання дія: {$action}';
